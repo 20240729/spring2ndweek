088de7e92de776c39f39d8c36215c7d24715028c
20240729 <yoohyeonsik@gmail.com>
2024-08-26 19:48:16 +0900

    di 우선 순위 설정 방법 세가지 - 줄 정렬

--- src/test/java/com/sparta/springauth/BeanTest.java
+++ src/test/java/com/sparta/springauth/BeanTest.java
@@ -11,9 +11,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 public class BeanTest {
 //    @Autowired
 //    Food food;
-    // 자동 주입 불가. 푸드 타입으로 빈이 한 개 이상.
-    // 푸드 필드에 같은 타입의 빈 객체가 하나 이상이라 어떤 타입에 주입할 지 몰라서.
-    // 치킨 혹은 피자
+// 자동 주입 불가. 푸드 타입으로 빈이 한 개 이상.
+// 푸드 필드에 같은 타입의 빈 객체가 하나 이상이라 어떤 타입에 주입할 지 몰라서.
+// 치킨 혹은 피자
 
 //    @Autowired
 //    Food pizza;
